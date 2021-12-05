7f4f0e1b1cca7f819e7506712d86064d3c495574
Michael Ma <temeraire02@gmail.com>
2021-12-05 11:39:45 -0800

    Fix migration file

--- db/migrate/20211204025910_remove_open_id_email_from_players.rb
+++ db/migrate/20211204025910_remove_open_id_email_from_players.rb
@@ -1,6 +1,5 @@
 class RemoveOpenIdEmailFromPlayers < ActiveRecord::Migration[6.1]
   def change
-    remove_column :players, :email
     remove_column :players, :open_id
     remove_column :players, :last_active_at
     rename_column :players, :avatar_url, :avatar
