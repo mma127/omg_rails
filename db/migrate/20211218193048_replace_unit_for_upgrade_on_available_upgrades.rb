b0462048d1f25bd522b3dbed72194a0871e2a839
Michael Ma <temeraire02@gmail.com>
2021-12-23 10:22:38 -0800

    Try fixing available upgrades migration again

--- db/migrate/20211218193048_replace_unit_for_upgrade_on_available_upgrades.rb
+++ db/migrate/20211218193048_replace_unit_for_upgrade_on_available_upgrades.rb
@@ -1,5 +1,4 @@
 class ReplaceUnitForUpgradeOnAvailableUpgrades < ActiveRecord::Migration[6.1]
   def change
-    add_reference :available_upgrades, :upgrade, index: true, foreign_key: true
   end
 end
