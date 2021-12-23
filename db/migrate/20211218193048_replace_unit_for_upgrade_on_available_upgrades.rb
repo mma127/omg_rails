c62054826223ffa429f93a1d73e9a9320cf5b75b
Michael Ma <temeraire02@gmail.com>
2021-12-23 10:14:15 -0800

    Fix available upgrades migration

--- db/migrate/20211218193048_replace_unit_for_upgrade_on_available_upgrades.rb
+++ db/migrate/20211218193048_replace_unit_for_upgrade_on_available_upgrades.rb
@@ -1,6 +1,5 @@
 class ReplaceUnitForUpgradeOnAvailableUpgrades < ActiveRecord::Migration[6.1]
   def change
-    remove_reference :available_upgrades,:unit
     add_reference :available_upgrades, :upgrade, index: true, foreign_key: true
   end
 end
