c2ebf8a5d2f7e74ec3f2651b2264a77ee754278a
Michael Ma <temeraire02@gmail.com>
2023-03-11 15:59:29 -0800

    offmaps seed csv

--- db/seeds/offmaps.seeds.rb
+++ db/seeds/offmaps.seeds.rb
@@ -1,9 +1,18 @@
 require 'csv'
 
 after :units do
-  def snakecase(str)
+  def snake_case(str)
     str.parameterize.underscore
   end
 
+  def title_case(str)
+    str.humanize.titleize
+  end
+
+  CSV.foreach('db/seeds/offmaps.csv', headers: true) do |row|
+    name = snake_case(row["internal_name"])
+    display_name = title_case(name)
+    Offmap.create!(name: name, display_name: display_name, const_name: row["const_name"])
+  end
 
 end
