126509ca42c0ad2290600df7b4a8332960e63803
Michael Ma <temeraire02@gmail.com>
2023-06-25 20:02:35 -0700

    added terror and blitz upgrades

--- app/javascript/constants/upgrades/wehrmacht.js
+++ app/javascript/constants/upgrades/wehrmacht.js
@@ -1,5 +1,7 @@
 import assault_grenades from '../../../assets/images/doctrines/wehrmacht/upgrades/assault_grenades.png'
+import bundle_grenade from '../../../assets/images/doctrines/wehrmacht/upgrades/bundle_grenade.png'
 import bunker from '../../../assets/images/doctrines/wehrmacht/upgrades/bunker.png'
+import camo_smocks from '../../../assets/images/doctrines/wehrmacht/upgrades/camo_smocks.png'
 import goliath from '../../../assets/images/doctrines/wehrmacht/upgrades/goliath.png'
 import grenade from '../../../assets/images/doctrines/wehrmacht/upgrades/grenade.png'
 import listening_post from '../../../assets/images/doctrines/wehrmacht/upgrades/listening_post.png'
@@ -10,23 +12,28 @@ import mg42_gunner from '../../../assets/images/doctrines/wehrmacht/upgrades/mg4
 import mg_bunker from '../../../assets/images/doctrines/wehrmacht/upgrades/mg_bunker.png'
 import mortar_bunker from '../../../assets/images/doctrines/wehrmacht/upgrades/mortar_bunker.png'
 import mp40 from '../../../assets/images/doctrines/wehrmacht/upgrades/mp40.png'
+import mp44 from '../../../assets/images/doctrines/wehrmacht/upgrades/mp44.png'
 import pantherturm from '../../../assets/images/doctrines/wehrmacht/upgrades/pantherturm.png'
 import panzerschreck from '../../../assets/images/doctrines/wehrmacht/upgrades/panzerschreck.png'
 import panzerfaust from '../../../assets/images/doctrines/wehrmacht/upgrades/panzerfaust.png'
 import puma_upgun from '../../../assets/images/doctrines/wehrmacht/upgrades/puma_upgun.png'
 import repair_bunker from '../../../assets/images/doctrines/wehrmacht/upgrades/repair_bunker.png'
 import skirts from '../../../assets/images/doctrines/wehrmacht/upgrades/skirts.png'
+import smoke_grenade from '../../../assets/images/doctrines/wehrmacht/upgrades/smoke_grenade.png'
+import tank_smoke from '../../../assets/images/doctrines/wehrmacht/upgrades/tank_smoke.png'
 
 import charismatic_leaders from '../../../assets/images/doctrines/wehrmacht/unlocks/defensive/charismatic_leaders.png'
 import g43 from '../../../assets/images/doctrines/panzer_elite/upgrades/g43.png'
 
+import repair_manuals from '../../../assets/images/doctrines/americans/upgrades/repair_manuals.png'
+
 import flamethrower from '../../../assets/images/doctrines/common/upgrades/flamethrower.png'
 import mine_detector from '../../../assets/images/doctrines/common/upgrades/mine_detector.png'
 import mines from '../../../assets/images/doctrines/common/upgrades/mines.png'
-import smoke_grenade from '../../../assets/images/doctrines/common/upgrades/smoke_grenade.png'
 import timed_demo from '../../../assets/images/doctrines/common/upgrades/timed_demo.png'
 
 export const ASSAULT_GRENADES = 'assault_grenades'
+export const AXIS_AT_MINES = 'axis_at_mines'
 export const AXIS_FLAMETHROWER = 'axis_flamethrower'
 export const AXIS_GOLIATH = 'axis_goliath'
 export const AXIS_GRENADE = 'axis_grenade'
@@ -35,10 +42,15 @@ export const AXIS_MINES = 'axis_mines'
 export const AXIS_PANZERFAUST = 'axis_panzerfaust'
 export const AXIS_PANZERSCHRECK = 'axis_panzerschreck'
 export const AXIS_SMOKE_GRENADE = 'axis_smoke_grenade'
+export const AXIS_TANK_SMOKE = 'axis_tank_smoke'
 export const AXIS_TIMED_DEMO = 'axis_timed_demo'
 
+export const BUNDLE_GRENADE = 'bundle_grenade'
 export const BUNKER = 'bunker'
+export const CAMO_SMOCKS = 'camo_smocks'
 export const CHARISMATIC_LEADERS = 'charismatic_leaders'
+export const CREW_REPAIR_KIT = 'crew_repair_kit'
+export const FOUR_MP44 = 'four_mp44'
 export const LISTENING_POST = 'listening_post'
 export const LMG42 = 'lmg42'
 export const MEDIC_BUNKER = 'medic_bunker'
@@ -56,10 +68,16 @@ export const PUMA_UPGUN = 'puma_upgun'
 export const REPAIR_BUNKER = 'repair_bunker'
 export const STUG_MG42 = 'stug_mg42'
 export const STUG_SKIRTS = 'stug_skirts'
+export const STUH_MG42 = 'stuh_mg42'
+export const STUH_SKIRTS = 'stuh_skirts'
 export const SUPPORT_KARBINES = 'support_karbines'
+export const ST_SCHRECK = 'st_schreck'
+export const TERROR_KARBINES = 'terror_karbines'
+export const TWO_MP44 = 'two_mp44'
 
 export const upgradeImageMapping = {
   [ASSAULT_GRENADES]: assault_grenades,
+  [AXIS_AT_MINES]: mines,
   [AXIS_FLAMETHROWER]: flamethrower,
   [AXIS_GOLIATH]: goliath,
   [AXIS_GRENADE]: grenade,
@@ -68,9 +86,14 @@ export const upgradeImageMapping = {
   [AXIS_PANZERFAUST]: panzerfaust,
   [AXIS_PANZERSCHRECK]: panzerschreck,
   [AXIS_SMOKE_GRENADE]: smoke_grenade,
+  [AXIS_TANK_SMOKE]: tank_smoke,
   [AXIS_TIMED_DEMO]: timed_demo,
+  [BUNDLE_GRENADE]: bundle_grenade,
   [BUNKER]: bunker,
+  [CAMO_SMOCKS]: camo_smocks,
   [CHARISMATIC_LEADERS]: charismatic_leaders,
+  [CREW_REPAIR_KIT]: repair_manuals,
+  [FOUR_MP44]: mp44,
   [LISTENING_POST]: listening_post,
   [LMG42]: lmg42,
   [MEDIC_BUNKER]: medic_bunker,
@@ -88,5 +111,10 @@ export const upgradeImageMapping = {
   [REPAIR_BUNKER]: repair_bunker,
   [STUG_MG42]: mg42_gunner,
   [STUG_SKIRTS]: skirts,
+  [STUH_MG42]: mg42_gunner,
+  [STUH_SKIRTS]: skirts,
   [SUPPORT_KARBINES]: g43,
+  [ST_SCHRECK]: panzerschreck,
+  [TERROR_KARBINES]: g43,
+  [TWO_MP44]: mp44,
 }
