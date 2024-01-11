f7ae47f43ff229a2b1c58e8427bb443998671585
Michael Ma <temeraire02@gmail.com>
2024-01-10 18:38:29 -0800

    set starting vps to 20 for testing

--- db/seeds/rulesets.seeds.rb
+++ db/seeds/rulesets.seeds.rb
@@ -3,6 +3,6 @@ Ruleset.create!(name: "war",
                 starting_man: 9000,
                 starting_mun: 1900,
                 starting_fuel: 1400,
-                starting_vps: 25, # FIXME: restore to 15
+                starting_vps: 20, # FIXME: restore to 15
                 max_vps: 25,
                 max_resource_bonuses: 5)
