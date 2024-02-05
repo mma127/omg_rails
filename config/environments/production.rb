1554ba14cbb477b2724af0360ef5b1be43025e54
Michael Ma <temeraire02@gmail.com>
2024-02-04 22:59:59 -0800

    update prod cache store

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,11 @@ Rails.application.configure do
   config.log_tags = [ :request_id ]
 
   # Use a different cache store in production.
-  config.cache_store = :mem_cache_store
+  if ENV["MEMCACHEDCLOUD_SERVERS"].present?
+    config.cache_store = :mem_cache_store
+  else
+    config.cache_store = :memory_store
+  end
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
