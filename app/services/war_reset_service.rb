391d5752dd5ab199156226e0f38b9e2e9623d846
Michael Ma <temeraire02@gmail.com>
2024-04-12 23:13:54 -0700

    Upgrade seeders

--- app/services/war_reset_service.rb
+++ app/services/war_reset_service.rb
@@ -3,16 +3,20 @@ class WarResetService < ApplicationService
   # IMPORTANT: Use this method to conduct a war reset.
   # This method orchestrates all required data seeding in order, particularly for data that requires a ruleset association
   def self.reset_ruleset(ruleset_type, name)
-    new_ruleset = Seeds::RulesetSeeder.create_new_ruleset(ruleset_type, name)
-    Seeds::FactionsSeeder.update_or_create_all # Also creates faction restrictions
-    Seeds::DoctrinesSeeder.update_or_create_all # Also creates doctrine restrictions
-    Seeds::UnitsSeeder.create_or_update_all # Also creates TransportAllowedUnits and UnitVet
+    ActiveRecord::Base.transaction do
+      new_ruleset = Seeds::RulesetSeeder.create_new_ruleset(ruleset_type, name)
+      Seeds::FactionsSeeder.update_or_create_all # Also creates faction restrictions
+      Seeds::DoctrinesSeeder.update_or_create_all # Also creates doctrine restrictions
+      Seeds::UnitsSeeder.create_or_update_all # Also creates TransportAllowedUnits and UnitVet
+      Seeds::UpgradesSeeder.create_or_update_all
 
-    Seeds::ResourceBonusesSeeder.create_for_ruleset(new_ruleset)
-    Seeds::UnlocksSeeder.create_for_ruleset(new_ruleset)
-    Seeds::OffmapsSeeder.create_for_ruleset(new_ruleset)
-    Seeds::CallinModifiersSeeder.create_for_ruleset(new_ruleset)
+      Seeds::ResourceBonusesSeeder.create_for_ruleset(new_ruleset)
+      Seeds::UnlocksSeeder.create_for_ruleset(new_ruleset)
+      Seeds::OffmapsSeeder.create_for_ruleset(new_ruleset)
+      Seeds::CallinModifiersSeeder.create_for_ruleset(new_ruleset)
 
-    Seeds::RestrictionUnitsSeeder.new(new_ruleset).create_for_ruleset
+      Seeds::RestrictionUnitsSeeder.new(new_ruleset).create_for_ruleset # Also creates unit swaps
+      Seeds::RestrictionUpgradesSeeder.new(new_ruleset).create_for_ruleset # Also creates upgrade swaps
+    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_04_11_062707) do
+ActiveRecord::Schema.define(version: 2024_04_13_001847) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -698,6 +698,7 @@ ActiveRecord::Schema.define(version: 2024_04_11_062707) do
     t.string "type", null: false, comment: "Type of Upgrade"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["name"], name: "index_upgrades_on_name", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
