3663ccecf262b7a9f1bda6bb1e3511f8db7c6507
Michael Ma <temeraire02@gmail.com>
2024-06-09 17:47:59 -0700

    show admin nav link only for admins

--- app/javascript/app/Navbar.jsx
+++ app/javascript/app/Navbar.jsx
@@ -3,11 +3,16 @@ import { Link, useLocation } from "react-router-dom";
 import { PlayerAuthStatus } from "../features/player/PlayerAuthStatus";
 import { AppBar, Box, Tab, Tabs, Toolbar } from "@mui/material";
 import logo from "../../assets/images/omg-logo.png"
+import { useSelector } from "react-redux";
+import { selectPlayer } from "../features/player/playerSlice";
 
 export const Navbar = () => {
   // https://stackoverflow.com/questions/70121657/react-material-ui-handle-nav-tabs-when-no-children-match-the-provided-value/70149889
   const location = useLocation();
   const [[, currentRoot]] = location.pathname.matchAll(/^(\/[^/]*)/g);
+
+  const player = useSelector(selectPlayer)
+  const isAdmin = player ? player.role === "admin" : false
 
   return (
     <AppBar position="static">
@@ -21,7 +26,7 @@ export const Navbar = () => {
           <Tab label="Restrictions" value="/restrictions" component={Link} to="/restrictions"/>
           <Tab label="Leaderboards" value="/leaderboards" component={Link} to="/leaderboards"/>
           <Tab label="Download" href="https://data.omgmod.net/OMG_Updater_v2.1.0.0.zip" download/>
-          <Tab label="Admin" to="/admin" target="_blank" rel="noopener noreferrer" component={Link}/>
+          {isAdmin ? <Tab label="Admin" to="/admin" target="_blank" rel="noopener noreferrer" component={Link}/> : null}
         </Tabs>
         <PlayerAuthStatus/>
       </Toolbar>
